79fcfa96b5dc2423814f2642286bc75cfe99b1ed
youngplq10 <patryk.plq@o2.pl>
2025-01-08 14:30:09 +0100

    little changes in entities

--- src/main/java/dev/starzynski/audiostore/Entity/Audiobook.java
+++ src/main/java/dev/starzynski/audiostore/Entity/Audiobook.java
@@ -1,17 +1,22 @@
 package dev.starzynski.audiostore.Entity;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-@Document(collation = "audiobook")
+@Document(collection = "audiobook")
 public class Audiobook {
 
     @Id
     private ObjectId id;
+    @Setter @Getter
     private String title;
+    @Setter @Getter
     private String author;
+    @Setter @Getter
     private String cover;
+    @Setter @Getter
     private String audioLink;
-
 }
